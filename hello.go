e6bec3317157f6fe456b03dd75a809400d783922
Naman Gera <namangera15@gmail.com>
2021-01-22 19:45:35 +0530

    complete - Hello, World

--- hello.go
+++ hello.go
@@ -4,10 +4,34 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "spanish"
+const french = "french"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+// Public function; so starts with an upper case.
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World!"
+	}
+
+	if name == "there" {
+		name = "there!"
+	}
+
+	mainPrefix := englishHelloPrefix
+
+	switch lang {
+	case french:
+		mainPrefix = frenchHelloPrefix
+	case spanish:
+		mainPrefix = spanishHelloPrefix
+	}
+
+	return mainPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "english"))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,19 +3,47 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World!")
-	want := "Hello, World!"
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		// for helper functions, it's a good idea to accept a testing.TB
+		// which acts as an interface that both *testing.T & *testing.B both
+		// satisfy, so u can call helper functions from a test or a benchmark.
+		if got != want {
+			t.Errorf("got %q but wanted %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Naman!", "english")
+		want := "Hello, Naman!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to hello world", func(t *testing.T) {
+		got := Hello("", "english")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Nimmo!", "spanish")
+		want := "Hola, Nimmo!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Nimmo!", "french")
+		want := "Bonjour, Nimmo!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func TestHelloAgain(t *testing.T) {
+	got := Hello("there", "english")
+	want := "Hello, there!"
 
 	if got != want {
 		t.Errorf("got %q but wanted %q", got, want)
 	}
 }
-
-// func TestHelloAgain(t *testing.T) {
-// 	got := Hello("world")
-// 	want := "Jello World!"
-
-// 	if got != want {
-// 		t.Errorf("got %q but wanted %q", got, want)
-// 	}
-// }
